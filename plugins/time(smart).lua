e2b170ba290a813df08b6deb496eba6456a8a95c
SmartCliBot <hosseinrnofficial@yahoo.com>
2016-12-24 23:23:14 +0330

    Update time(smart).lua

--- plugins/time(smart).lua
+++ plugins/time(smart).lua
@@ -7,7 +7,7 @@ local colors = {'blue','green','yellow','magenta','Orange','DarkOrange','red'}
 local fonts = {'mathbf','mathit','mathfrak','mathrm'}
 local jdat = json:decode(url)
 local url = 'http://latex.codecogs.com/png.download?'..'\\dpi{600}%20\\huge%20\\'..fonts[math.random(#fonts)]..'{{\\color{'..colors[math.random(#colors)]..'}'..jdat.ENtime..'}}'
-local text = '⏱ ساعت '..jdat.FAtime..' \n🔖 امروز '..jdat.FAdate..' میباشد.\n--------------\n⏱ '..jdat.ENtime..'\n🔖 '..jdat.ENdate.. '\n#Smart\n--------------\n🔺 کانال اسمارت: @Radicall_team 🔻'
+local text = '⏱ ساعت '..jdat.FAtime..' \n🔖 امروز '..jdat.FAdate..' میباشد.\n--------------\n⏱ '..jdat.ENtime..'\n🔖 '..jdat.ENdate.. '\n#Smart\n--------------\n🔺 کانال تله بات استور: @TeleBotStore 🔻'
 local file = download_to_file(url,'time.webp')
 send_document(get_receiver(msg) , file, ok_cb, false)
 return  reply_msg(msg.id, text, ok_cb, false)
